cd674ac75068c94705b562e15ed09086af7fc8c1
喜明 廖 <1435646097@qq.com>
2019-09-29 10:22:05 +0800

    优化自动登录

--- Web/Login.aspx.cs
+++ Web/Login.aspx.cs
@@ -54,14 +54,6 @@ namespace BookShop.Web
                 if (userManager.UserLogin(name, pwd, out user, out msg))
                 {
                     Session["userLogin"] = user;
-                    if (!string.IsNullOrEmpty(Context.Request["returnUrl"]))//判断是否是从别的页面跳转到登录界面
-                    {
-                        Response.Redirect(Context.Request["returnUrl"]);
-                    }
-                    else
-                    {
-                        Response.Redirect("/UserCenter.aspx");//跳转到用户中心
-                    }
                     if (!string.IsNullOrEmpty(Request.Form["chkRember"]))//勾选了自动登录
                     {
                         HttpCookie cookie1 = new HttpCookie("cp1", name);
@@ -70,6 +62,14 @@ namespace BookShop.Web
                         cookie2.Expires = DateTime.Now.AddDays(7);
                         Response.Cookies.Add(cookie1);
                         Response.Cookies.Add(cookie2);
+                    }
+                    if (!string.IsNullOrEmpty(Context.Request["returnUrl"]))//判断是否是从别的页面跳转到登录界面
+                    {
+                        Response.Redirect(Context.Request["returnUrl"]);
+                    }
+                    else
+                    {
+                        Response.Redirect("/UserCenter.aspx");//跳转到用户中心
                     }
                 }
                 else
